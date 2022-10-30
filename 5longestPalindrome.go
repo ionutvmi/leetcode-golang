684fcba92a133e8981f5cfba2adc649d419369e8
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-10-30 12:15:08 +0200

    Create 5longestPalindrome.go

---
+++ 5longestPalindrome.go
@@ -0,0 +1,33 @@
+import "strings"
+
+func longestPalindrome(s string) string {
+    
+    chars := strings.Split(s, "")
+    result := ""
+    
+    for index := range chars {
+        oddResult:= expandPalindrome(chars, index, index)
+        evenResult := expandPalindrome(chars, index, index+1)
+        
+        if len(result) < len(oddResult) {
+            result = oddResult
+        }
+        
+        if len(result) < len(evenResult) {
+            result = evenResult
+        }
+    }
+    
+    return result
+}
+
+func expandPalindrome(chars []string, left int, right int) string {
+    size := len(chars)
+
+    for left >= 0 && right < size && chars[left] == chars[right] {
+        left--
+        right++
+    }
+    
+    return strings.Join(chars[left+1:right], "")
+}
