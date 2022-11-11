eadbf425d8aa07e0ece665e39b34943b7bae8dd3
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-11-11 14:25:41 +0200

    Create 12IntegerToRoman.go

---
+++ 12IntegerToRoman.go
@@ -0,0 +1,79 @@
+import "strings"
+
+
+func intToRoman(num int) string {
+    result := ""
+    reminder := num
+    
+    if reminder >= 1000 {
+        result += strings.Repeat("M", int(num / 1000))
+        reminder = reminder % 1000
+    }
+
+    if reminder >= 900 {
+        result += "CM"
+        
+        reminder = reminder % 900
+    }
+
+    
+    if reminder >= 500 {
+        result += "D"
+        
+        reminder = reminder % 500
+    }
+    
+    if reminder >= 400 {
+        result += "CD"
+        
+        reminder = reminder % 400
+    }
+    
+    if reminder >= 100 {
+        result += strings.Repeat("C", int(reminder / 100))
+        reminder = reminder % 100
+    }
+    
+    if reminder >= 90 {
+        result += "XC"
+        
+        reminder = reminder % 90
+    }
+    
+    if reminder >= 50 {
+        result += "L"
+        
+        reminder = reminder % 50
+    }
+    
+    if reminder >= 40 {
+        result += "XL"
+        
+        reminder = reminder % 40
+    }
+    
+    if reminder >= 10 {
+        result += strings.Repeat("X", int(reminder / 10))
+        reminder = reminder % 10
+    }
+    
+    if reminder == 9 {
+        result += "IX"
+        reminder -= 9
+    }
+    
+    if reminder >= 5 {
+        result += "V"
+        reminder = reminder % 5
+    }
+    
+    if reminder == 4 {
+        result += "IV"
+        reminder -= 4
+    }
+
+    result += strings.Repeat("I", reminder)
+
+    return result
+}
+
